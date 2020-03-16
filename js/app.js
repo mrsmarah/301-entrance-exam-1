f864007e5e9660d234cef89a3f9b5cbb081016ce
mrsmarah <marahabdallah91@gmail.com>
2020-03-16 17:02:59 +0200

    fe

--- js/app.js
+++ js/app.js
@@ -10,26 +10,22 @@ tasks.push(this);
 
 // console.log('hii');
 
-var eventOne = document.getElementById("formData");
+var eventOne = document.getElementById("submit-btn");
 eventOne.addEventListener('click', clickbtn);
 
 function clickbtn(e){
-    // console.log('hii');
 
     e.preventDefault();
-    // console.log('hii');
     if ((document.getElementById("taskInput").value !== '') && (document.getElementById("dateInput").value !== '')){
         console.log('hi');
         new Task(document.getElementById("taskInput").value ,document.getElementById("dateInput").value );
-        // render();
-        // getItem();
-
+    
     }
-    console.log(tasks);
-            // render();
             setItem();
-            getItem();
+            render();
 
+            document.getElementById("taskInput").value = '';
+            document.getElementById("dateInput").value = '';
 }
 function setItem(){
     var objStorage = JSON.stringify(tasks);
@@ -39,10 +35,13 @@ function getItem(){
     var objStorage = localStorage.getItem('objStorage');
     if (objStorage){
         tasks = JSON.parse(objStorage);
+        // render();
+
     }
 }
 
 function render(){
+    getItem();
 var container = document.getElementById("list");
 var ulEL = document.createElement('ul');
 // ulEl.textContent='';
@@ -53,5 +52,3 @@ for(var i=0; i<tasks.length ; i++){
     liEl.textContent=`${tasks[i].name}`
 }
 }
-getItem();
-render();
